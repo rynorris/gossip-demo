d4fd96bfeac93840ed619d9d588688d8eea0c39c
Ryan Norris <rynorris@gmail.com>
2014-11-05 16:19:03 +0000

    Add endpoint struct to make main code waaay cleaner.

---
+++ endpoint.go
@@ -0,0 +1,105 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/stefankopieczek/gossip/base"
+	"github.com/stefankopieczek/gossip/log"
+	"github.com/stefankopieczek/gossip/transaction"
+)
+
+type endpoint struct {
+	// Sip Params
+	displayName string
+	username    string
+	host        string
+
+	// Transport Params
+	addr      string // Listens on and sends from this address.
+	port      uint16 // Listens on this port.
+	transport string // Sends using this transport. ("tcp" or "udp")
+
+	// Internal guts
+	tm *transaction.Manager
+}
+
+func (e *endpoint) Start() error {
+	tm, err := transaction.NewManager(e.transport, fmt.Sprintf("%v:%v", e.addr, e.port))
+	if err != nil {
+		return err
+	}
+
+	e.tm = tm
+
+	return nil
+}
+
+func (caller *endpoint) Invite(callee *endpoint) {
+	branch := "z9hG4bK.callbranch1"
+	callid := base.CallId("thisisacall3")
+	invite := base.NewRequest(
+		base.INVITE,
+		&base.SipUri{
+			User: &callee.username,
+			Host: callee.host,
+		},
+		"SIP/2.0",
+		[]base.SipHeader{
+			&base.ViaHeader{
+				&base.ViaHop{
+					ProtocolName:    "SIP",
+					ProtocolVersion: "2.0",
+					Transport:       caller.transport,
+					Host:            caller.addr,
+					Port:            &caller.port,
+					Params: base.Params{
+						"branch": &branch,
+					},
+				},
+			},
+			&base.ToHeader{
+				Address: &base.SipUri{
+					User: &callee.username,
+					Host: callee.host,
+				},
+			},
+			&base.FromHeader{
+				Address: &base.SipUri{
+					User: &caller.username,
+					Host: caller.host,
+					UriParams: base.Params{
+						"transport": &caller.transport,
+					},
+				},
+			},
+			&base.ContactHeader{
+				Address: &base.SipUri{
+					User: &caller.username,
+					Host: caller.host,
+				},
+			},
+			&base.CSeq{
+				SeqNo:      1,
+				MethodName: base.INVITE,
+			},
+			&callid,
+			base.ContentLength(0),
+		},
+		"",
+	)
+
+	tx := caller.tm.Send(invite, fmt.Sprintf("%v:%v", callee.host, callee.port))
+	for {
+		select {
+		case r := <-tx.Responses():
+			log.Info(r.String())
+			if r.StatusCode < 300 && r.StatusCode >= 200 {
+				// Ack 200s manually.
+				log.Info("Sending Ack")
+				tx.Ack()
+			}
+		case e := <-tx.Errors():
+			log.Warn(e.Error())
+		}
+	}
+}
--- main.go
+++ main.go
@@ -1,102 +1,37 @@
 package main
 
 import (
-	"fmt"
-
-	"github.com/stefankopieczek/gossip/base"
 	"github.com/stefankopieczek/gossip/log"
-	"github.com/stefankopieczek/gossip/transaction"
 )
 
 var (
-	// Call parameters
-	transport = "UDP"
-	callid    = base.CallId("thisisacall3")
-	branch    = "z9hG4bK.callbranch1"
-
 	// Caller parameters
-	caller             = "ryan"
-	src_host           = "centosvm-rpn"
-	listen_port uint16 = 5060
+	caller = &endpoint{
+		displayName: "Ryan",
+		username:    "ryan",
+		host:        "centosvm-rpn",
+		addr:        "centosvm-rpn",
+		port:        5060,
+		transport:   "TCP",
+	}
 
 	// Callee parameters
-	callee          = "stefan"
-	dst_host        = "172.18.115.72"
-	dst_port uint16 = 5060
+	callee = &endpoint{
+		displayName: "Stefan",
+		username:    "stefan",
+		host:        "PC4470",
+		addr:        "PC4470",
+		port:        5060,
+		transport:   "TCP",
+	}
 )
 
 func main() {
-	log.SetDefaultLogLevel(log.DEBUG)
-	// Build an INVITE to send.
-	invite := base.NewRequest(
-		base.INVITE,
-		&base.SipUri{
-			User: &callee,
-			Host: dst_host,
-		},
-		"SIP/2.0",
-		[]base.SipHeader{
-			&base.ViaHeader{
-				&base.ViaHop{
-					ProtocolName:    "SIP",
-					ProtocolVersion: "2.0",
-					Transport:       transport,
-					Host:            dst_host,
-					Port:            &dst_port,
-					Params: base.Params{
-						"branch": &branch,
-					},
-				},
-			},
-			&base.ToHeader{
-				Address: &base.SipUri{
-					User: &callee,
-					Host: dst_host,
-				},
-			},
-			&base.FromHeader{
-				Address: &base.SipUri{
-					User: &caller,
-					Host: src_host,
-					UriParams: base.Params{
-						"transport": &transport,
-					},
-				},
-			},
-			&base.ContactHeader{
-				Address: &base.SipUri{
-					User: &caller,
-					Host: src_host,
-				},
-			},
-			&base.CSeq{
-				SeqNo:      1,
-				MethodName: base.INVITE,
-			},
-			&callid,
-			base.ContentLength(0),
-		},
-		"",
-	)
-
-	tm, err := transaction.NewManager(transport, fmt.Sprintf("%v:%v", src_host, listen_port))
+	err := caller.Start()
 	if err != nil {
-		panic(err)
+		log.Warn("Failed to start caller: %v", err)
+		return
 	}
 
-	tx := tm.Send(invite, fmt.Sprintf("%v:%v", dst_host, dst_port))
-
-	for {
-		select {
-		case r := <-tx.Responses():
-			log.Info(r.String())
-			if r.StatusCode < 300 && r.StatusCode >= 200 {
-				// Ack 200s manually.
-				log.Info("Sending Ack")
-				tx.Ack()
-			}
-		case e := <-tx.Errors():
-			log.Warn(e.Error())
-		}
-	}
+	caller.Invite(callee)
 }
